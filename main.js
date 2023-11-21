f2851fb63ddcd14d37eb866c13704f75e17711b4
Roque Alves <roquedev23@gmail.com>
2023-11-21 00:57:57 -0300

    commit incial

---
+++ main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $('header button').click(function(e) {
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function(){
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const endercoDaNovaImagem =  $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${endercoDaNovaImagem}" />`).appendTo(novoItem);
+        $(`<div class"overlay-imagem-link">
+            <a href="${endercoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real"
+            Ver imagem em tamanho real
+        </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereco-imagem-nova').val('');
+    })
+})
